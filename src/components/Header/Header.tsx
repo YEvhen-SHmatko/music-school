7e07162cac4795ecb0bbfad0bc4c6e0d42fb74f5
irynahamoum <iryshka.hamoum@gmail.com>
2022-10-13 16:46:56 +0200

    changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Avatar, Button, Col, Row, Space, Typography } from 'antd';
+import { UserOutlined, MenuOutlined } from '@ant-design/icons';
+import Icon from '../../assets/icon.svg';
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <Row style={{ padding: '12px 28px' }}>
+      <Col flex="134px">
+        <Space wrap={false}>
+          <Avatar size={30} icon={<img src={Icon} alt="test" />} />
+          <Typography.Title style={{ fontSize: 8, textTransform: 'uppercase' }}>
+            ПОЛТАВСЬКА ДИТЯЧА МУЗИЧНА ШКОЛА No 2 ІМ. В.П. ШАПОВАЛЕНКА{' '}
+          </Typography.Title>
+        </Space>
+      </Col>
+      <Col flex="auto">
+        <Row justify="end" align="middle" gutter={8}>
+          <Col>
+            <UserOutlined />
+          </Col>
+          <Col>UK</Col>
+          <Col>
+            <Button style={{ background: '#E0E0E0' }} icon={<MenuOutlined />} />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 };
 
 export default Header;
