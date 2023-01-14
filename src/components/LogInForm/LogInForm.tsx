61d844cc368917458f270ad07b899a805dcef0a6
irynahamoum <iryshka.hamoum@gmail.com>
2023-01-14 15:24:08 +0100

    new changes

--- src/components/LogInForm/LogInForm.tsx
+++ src/components/LogInForm/LogInForm.tsx
@@ -1,6 +1,6 @@
 import { Button, Form, Input } from 'antd';
 import React from 'react';
-
+import s from './loginform.module.css';
 const App: React.FC = () => {
   const onFinish = (values: any) => {
     console.log('Success:', values);
@@ -12,39 +12,35 @@ const App: React.FC = () => {
 
   return (
     <Form
+      className={s.container}
       name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       autoComplete="off"
     >
-      <div className="s.container">
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={'email'}
-          label="Email"
-          rules={[{ type: 'email', required: true, message: 'Please input your Email!' }]}
-        >
-          <Input />
-        </Form.Item>
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name={'email'}
+        label="Email"
+        rules={[{ type: 'email', required: true, message: 'Please input your Email!' }]}
+      >
+        <Input />
+      </Form.Item>
 
-        <Form.Item label="коментар" name="coment">
-          <Input />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Call me back
-          </Button>
-        </Form.Item>
-      </div>
+      <Form.Item label="коментар" name="coment">
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Call me back
+        </Button>
+      </Form.Item>
     </Form>
   );
 };
