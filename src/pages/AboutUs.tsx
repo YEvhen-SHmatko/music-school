78554f3a79e9f259c785da7f3015ac873cbe487f
Yevhen Shmatko <yevhen.shmatko@gmail.com>
2023-03-23 20:21:54 +0200

    feat: add map to page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,13 +1,18 @@
 import { Col, Row } from 'antd';
 import React from 'react';
-import { Quote } from '../components';
+import { Label, Map, Quote } from '../components';
 
 const AboutUs: React.FC = () => (
   <Row>
     <Col span={24}>
       <Quote quoteText="Every art tends to become music" autor="Peter Walter" />
     </Col>
-    AboutUs
+    <Col span={24}>
+      <div className="root-container">
+        <Label labelText="Як нас знайти" />
+        <Map width="100%" height="400px" />
+      </div>
+    </Col>
     <Col span={24}>
       <Quote
         quoteText="Музика - могутнє джерело думки. Без музичного виховання неможливий повноцінний розумовий розвиток."
